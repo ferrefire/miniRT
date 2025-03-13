99a098dbe549c5da4ff8a0ebe92b0cc327555bd6
Ferre <ferremolenbeek@gmail.com>
2025-03-13 02:29:38 +0100

    A lot of optimization and performance increases!

--- sources/intersecting.c
+++ sources/intersecting.c
@@ -6,24 +6,25 @@
 /*   By: ferre <ferre@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/10 22:49:31 by ferre         #+#    #+#                 */
-/*   Updated: 2025/03/12 23:29:29 by ferre         ########   odam.nl         */
+/*   Updated: 2025/03/13 02:12:05 by ferre         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "intersecting.h"
 #include "vector.h"
+#include "utilities.h"
 
 int intersectingSphere(t_vec point, t_sphere sphere)
 {
 	float centerDistance;
 
-	centerDistance = distance(point, sphere.center);
-	if (centerDistance <= sphere.radius)
-		return (1);
-	//centerDistance = distanceSquared(point, sphere.center);
-	//if (centerDistance <= sphere.radius * sphere.radius) 
+	//centerDistance = distance(point, sphere.center);
+	//if (centerDistance <= sphere.radius)
 	//	return (1);
+	centerDistance = distanceSquared(point, sphere.center);
+	if (centerDistance <= sphere.radius * sphere.radius) 
+		return (1);
 	return (0);
 }
 
@@ -65,28 +66,33 @@ t_hit intersectingCylinder(t_vec point, t_cylinder cylinder)
 
 float closestSphere(t_vec point, t_scene_data scene)
 {
-	t_vec closestSphere;
+	//t_vec closestSphere;
+	//float closestRadius;
 	float closestDistance;
 
 	closestDistance = 10000.0;
 	for (int i = 0; i < scene.shapes.sphereCount; i++)
 	{
-		float currentDistance = distanceSquared(point, scene.shapes.spheres[i].center);
+		float currentDistance = distanceSquared(point, scene.shapes.spheres[i].center) - 
+			(scene.shapes.spheres[i].radius * scene.shapes.spheres[i].radius);
 		if (currentDistance < closestDistance)
 		{
 			closestDistance = currentDistance;
-			closestSphere = scene.shapes.spheres[i].center;
+			//closestSphere = scene.shapes.spheres[i].center;
+			//closestRadius = scene.shapes.spheres[i].radius;
 		}
 	}
 
-	if (closestDistance < 10000.0)
-		return (distance(point, closestSphere));
+	//if (closestDistance < 10000.0)
+	//	return (distance(point, closestSphere) - closestRadius);
+	return (closestDistance);
 }
 
 float closestCylinder(t_vec point, t_scene_data scene)
 {
-	t_vec closestCylinder;
+	//t_vec closestCylinder;
 	float closestDistance;
+	//float closestRadius;
 
 	closestDistance = 10000.0;
 	for (int i = 0; i < scene.shapes.cylinderCount; i++)
@@ -96,27 +102,61 @@ float closestCylinder(t_vec point, t_scene_data scene)
 		t_vec axisVec = mult(scene.shapes.cylinders[i].axis, t);
 		t_vec axisPoint = add(scene.shapes.cylinders[i].position, axisVec);
 
-		float currentDistance = distanceSquared(point, axisPoint);
+		float currentDistance = distanceSquared(point, axisPoint) -
+			(scene.shapes.cylinders[i].radius * scene.shapes.cylinders[i].radius);
 		if (currentDistance < closestDistance)
 		{
 			closestDistance = currentDistance;
-			closestCylinder = axisPoint;
+			//closestCylinder = axisPoint;
+			//closestRadius = scene.shapes.cylinders[i].radius;
 		}
 	}
 
-	if (closestDistance < 10000.0)
-		return (distance(point, closestCylinder));
+	//if (closestDistance < 10000.0)
+	//	return (distance(point, closestCylinder) - closestRadius);
+	return (closestDistance);
+}
+
+float closestPlane(t_vec point, t_scene_data scene)
+{
+	//t_vec closestPlane;
+	float closestDistance;
+
+	closestDistance = 10000.0;
+	for (int i = 0; i < scene.shapes.planeCount; i++)
+	{
+		float d = -(scene.shapes.planes[i].normal.x * scene.shapes.planes[i].position.x + 
+			scene.shapes.planes[i].normal.y * scene.shapes.planes[i].position.y + 
+			scene.shapes.planes[i].normal.z * scene.shapes.planes[i].position.z);
+		float side = scene.shapes.planes[i].normal.x * point.x + scene.shapes.planes[i].normal.y * 
+			point.y + scene.shapes.planes[i].normal.z * point.z + d;
+		float currentDistance = abso(side);
+		if (currentDistance < closestDistance)
+		{
+			closestDistance = currentDistance;
+		}
+	}
+
+	//if (closestDistance < 10000.0)
+	//	return (distance(point, closestPlane));
+	return (closestDistance * closestDistance * closestDistance);
 }
 
 float closestShape(t_vec point, t_scene_data scene)
 {
+	float nearestPlane;
 	float nearestSphere;
 	float nearestCylinder;
 
+	nearestPlane = closestPlane(point, scene);
+	if (nearestPlane <= 1.0) return (nearestPlane);
 	nearestSphere = closestSphere(point, scene);
+	if (nearestSphere <= 1.0) return (nearestSphere);
 	nearestCylinder = closestCylinder(point, scene);
 
-	if (nearestSphere < nearestCylinder)
+	if (nearestPlane < nearestSphere && nearestPlane < nearestCylinder)
+		return (nearestPlane);
+	else if (nearestSphere < nearestCylinder)
 		return (nearestSphere);
 	else
 		return (nearestCylinder);
--- sources/utilities.c
+++ sources/utilities.c
@@ -6,7 +6,7 @@
 /*   By: ferre <ferre@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/10 21:37:59 by ferre         #+#    #+#                 */
-/*   Updated: 2025/03/11 13:38:30 by ferre         ########   odam.nl         */
+/*   Updated: 2025/03/13 00:30:49 by ferre         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,14 @@ float clamp(float value, float min, float max)
 	if (value < min) return (min);
 	else if (value > max) return (max);
 	else return (value);
+}
+
+float abso(float value)
+{
+	if (value >= 0.0) 
+		return (value);
+	else
+		return (value * -1.0);
 }
 
 void	*allocate_memory(size_t buffer_size)
